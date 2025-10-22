b48ed60ea5d5d4c14b038fe54fd32df1b9976d8e
khwankamon <khwan_yyg@outlook.com>
2025-10-22 15:49:55 +0700

    create controller and connect database

---
+++ candidate-service/db/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
+  database: "candidate_master",
+  connectionLimit: 10,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) throw err;
+  console.log(`Connected to Mysql DB successfully!`);
+  connection.release();
+});
+
+module.exports = pool;
