dfdfd80a89295bd17a569ec9a42da06e412adeb2
khwankamon <khwan_yyg@outlook.com>
2025-10-27 17:09:48 +0700

    feat: add data table(Angular Material) and add,update,delete method

--- candidate-web/src/app/candidate/candidate.service.ts
+++ candidate-web/src/app/candidate/candidate.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Candidate } from './candidate';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CandidateService {
+  constructor(private _httpClient: HttpClient) {}
 
-  constructor() { }
+  baseUrl: String = '/api/v1/candidates';
+
+  fetchAllCandidates(): Observable<Candidate[]> {
+    return this._httpClient.get<Candidate[]>(`${this.baseUrl}`);
+  }
+
+  createCandidate(data: Candidate) {
+    return this._httpClient.post<Candidate>(`${this.baseUrl}`, data);
+  }
+
+  updateCandidate(data: Candidate) {
+    return this._httpClient.put<Candidate>(`${this.baseUrl}/${data.id}`, data);
+  }
+
+  deleteCandidate(id: Number) {
+    return this._httpClient.delete<Candidate>(`${this.baseUrl}/${id}`);
+  }
 }
--- candidate-web/src/app/candidate/home/home.component.ts
+++ candidate-web/src/app/candidate/home/home.component.ts
@@ -1,12 +1,123 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Candidate } from '../candidate';
+import { CandidateService } from '../candidate.service';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'email',
+    'address',
+    'edit',
+    'delete',
+  ];
+  dataSource = new MatTableDataSource<Candidate>();
+  @ViewChild(MatSort) sort: any;
+  @ViewChild(MatPaginator) paginator: any;
 
+  candidate: Candidate = {
+    id: 0,
+    name: '',
+    email: '',
+    address: '',
+  };
+  candidates: Candidate[] = [];
+  filteredCandidates: Candidate[] = [];
+
+  constructor(private candidateService: CandidateService) {}
+
+  ngAfterViewInit(): void {
+    this.candidateService.fetchAllCandidates().subscribe((data) => {
+      this.candidates = data;
+      this.dataSource = new MatTableDataSource<Candidate>(data);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  searchCandidate(input: String) {
+    this.filteredCandidates = this.candidates.filter(
+      (item) =>
+        item.name.toLowerCase().includes(input.toLowerCase()) ||
+        item.email.toLowerCase().includes(input.toLowerCase()) ||
+        item.address.toLowerCase().includes(input.toLowerCase())
+    );
+    this.dataSource = new MatTableDataSource<Candidate>(
+      this.filteredCandidates
+    );
+  }
+
+  addOrEditCandidate(candidate: Candidate) {
+    if (candidate.id !== 0) {
+      //update
+      this.candidateService.updateCandidate(candidate).subscribe({
+        next: (data) => {
+          console.log(` Candidate Updated Successfully!`);
+          window.location.reload();
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    } else {
+      this.candidateService.createCandidate(candidate).subscribe({
+        next: (data) => {
+          console.log(`New Candidate Created Successfully!`);
+          window.location.reload();
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
+
+  clearAll() {
+    this.candidate.name = '';
+    this.candidate.email = '';
+    this.candidate.address = '';
+  }
+
+  populateFormFields(candidate: Candidate) {
+    this.candidate.id = candidate.id;
+    this.candidate.name = candidate.name;
+    this.candidate.email = candidate.email;
+    this.candidate.address = candidate.address;
+  }
+
+  deleteCandidate(id: Number) {
+    const isConfirem = window.confirm('Are you sure you want to delete?');
+    if (isConfirem) {
+      this.candidateService.deleteCandidate(id).subscribe((data) => {
+        this.candidates = this.candidates.filter((item) => item.id !== id);
+      });
+      window.location.reload();
+    }
+  }
 }
